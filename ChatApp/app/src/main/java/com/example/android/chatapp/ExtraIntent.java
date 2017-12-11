535bf98e1591a84661fd26efefd353d6913cd2e9
nguyenvantinh_94@yahoo.com.vn <nguyenvantinh_94@yahoo.com.vn>
2017-12-11 08:51:40 +0700

    app chat

---
+++ ChatApp/app/src/main/java/com/example/android/chatapp/ExtraIntent.java
@@ -0,0 +1,11 @@
+package com.example.android.chatapp;
+
+/**
+ * Created by phamm on 12/7/2017.
+ */
+
+public class ExtraIntent {
+    public static final String EXTRA_CHAT_REF = "EXTRA_CHAT_REF";
+    public static final String EXTRA_CURRENT_USER_ID = "EXTRA_CURRENT_USER_ID";
+    public static final String EXTRA_RECIPIENT_ID = "EXTRA_RECIPIENT_ID";
+}
